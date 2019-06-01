c24ef99a50ade60f67697b7a3980da854a638f98
denis <denis.khodakovskiy@gmail.com>
2019-06-01 15:42:36 +0300

    Debian -> Ubuntu

--- Vagrant.provision.sh
+++ Vagrant.provision.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
-sudo apt install -y ca-certificates apt-transport-https
-wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
-echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
-sudo apt-get update
-
-sudo apt-get install -y php7.1 php7.1-cli php7.1-common php7.1-curl php7.1-fpm php7.1-gd php7.1-json php7.1-mbstring
-sudo apt-get install -y php7.1-mysql php7.1-soap php7.1-xml php7.1-xmlrpc php7.1-zip php7.1-bcmath php-amqplib
-sudo apt-get install -y nginx composer git nano
+sudo apt-get install -y php7.2 php7.2-cli php7.2-common php7.2-curl php7.2-fpm php7.2-gd php7.2-json php7.2-mbstring
+sudo apt-get install -y php7.2-mysql php7.2-soap php7.2-xml php7.2-xmlrpc php7.2-zip php7.2-bcmath php-amqplib
+sudo apt-get install -y nginx composer git nano mc
 sudo apt-get install -y redis-server
 sudo systemctl enable redis-server.service
-sudo apt-get install -y php7.1-redis
+sudo apt-get install -y php7.2-redis
 sudo apt-get install -y rabbitmq-server
 
-DATABASE_NAME='eveservice'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
-apt-get install -y mysql-server mysql-client mysql-common
-mysql -uroot -ppassword -e "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME;";
+sudo apt-get install -y mysql-server mysql-client mysql-common
+mysql -uroot -ppassword -e "CREATE DATABASE IF NOT EXISTS eveservice;";
 mysql -uroot -ppassword -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';"
 mysql -uroot -ppassword -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password';"
 sudo service mysql restart
@@ -32,14 +26,15 @@ git fetch origin
 git reset --hard origin/master
 cp /vagrant/provision/db/* /var/www/html/eve-service/config/
 ./yii migrate --interactive=0
+sudo rm composer.lock
 composer install
 
 scp -o 'StrictHostKeyChecking no' -i /vagrant/provision/ssh/fornex_rsa root@5.187.4.2:/tmp/eveservice.sql /tmp/eveservice.sql
 cd /tmp
 mysql -u root -ppassword eveservice < eveservice.sql
 
-sudo apt-get install -y php7.1-xdebug
-cat >> /etc/php/7.1/mods-available/xdebug.ini <<'EOF'
+sudo apt-get install -y php7.2-xdebug
+sudo cat >> /etc/php/7.2/mods-available/xdebug.ini <<'EOF'
 xdebug.remote_enable=1
 xdebug.remote_host=localhost
 xdebug.idekey=PHPSTORM
@@ -51,5 +46,5 @@ sudo touch /var/www/log/eve_error.log
 
 sudo service rabbitmq-server start
 sudo service nginx restart
-sudo service php7.1-fpm restart
+sudo service php7.2-fpm restart
 sudo service redis-server restart
